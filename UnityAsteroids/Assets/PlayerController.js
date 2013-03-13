e853d946a48146e15a4db2338d495dd465084f00
Gerry Said <gerrysaid@gmail.com>
2013-03-13 12:22:15 +0100

    fifth commit
    Added asteroids generator for random asteroids.
    Added border on asteroids.
    Implemented asteroids movement.
    Added 60 seconds timer for the game.
    Added sequence for asteroids generator.

--- UnityAsteroids/Assets/PlayerController.js
+++ UnityAsteroids/Assets/PlayerController.js
@@ -5,36 +5,60 @@ var turboSpeed:int;
 //this is the slot for the laser prefab
 var laserprefab:Rigidbody;
 
+//start time initialized to float and set to zero
+var startTime:float=0.0;
+var currentTime:float=0.0;
+var elapsedTime:int=0;
+
+
 
 
 function Start () {
-
+	//the time the player started playing
+	startTime=Time.time;
 }
 
 function Update () {
-
-	//enable borders using the borders function in BorderController
-	BorderController.EnableBorders(transform);
-
-	//rotation of the cube
-	transform.Rotate(Vector3.forward * -40 * Input.GetAxis("Horizontal") * Time.deltaTime);
+	currentTime = Time.time;
+	//il-hin li ghadda nahdmuh hekk:
 	
-	//fire is the left ctrl key
-	if(Input.GetKeyDown(KeyCode.LeftControl))
+	//kill the game after sixty seconds
+	if (elapsedTime < 60)
 	{
-		//create a laser at the position of the space ship.
-		Instantiate(laserprefab,transform.position,transform.rotation);
-	}
+		elapsedTime = currentTime - startTime;
+		//enable borders using the borders function in BorderController
+		BorderController.EnableBorders(transform);
+
+		//rotation of the cube
+		transform.Rotate(Vector3.forward * -40 * Input.GetAxis("Horizontal") * Time.deltaTime);
 	
-	//speed / turbo speed
-	if (Input.GetKey(KeyCode.Space))
-	{
-		transform.Translate(Vector3.up * turboSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
-	}
-	else
-	{
-		//if not holding space bar
-		transform.Translate(Vector3.up * normalSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+		//fire is the left ctrl key
+		if(Input.GetKeyDown(KeyCode.LeftControl))
+		{
+			//create a laser at the position of the space ship.
+			Instantiate(laserprefab,transform.position,transform.rotation);
+		}
+	
+		//speed / turbo speed
+		if (Input.GetKey(KeyCode.Space))
+		{
+			transform.Translate(Vector3.up * turboSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+		}
+		else
+		{
+			//if not holding space bar
+			transform.Translate(Vector3.up * normalSpeed * Input.GetAxis("Vertical") * Time.deltaTime);
+		}
+		
+	} else {
+	
+		//game over
+		print("game over");
 	}
 
+}
+
+function OnGUI()
+{
+	GUI.Label(Rect(0,0,150,50),"Elapsed Time: "+elapsedTime);
 }
